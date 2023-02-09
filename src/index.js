1ae3f5912f2ea954ea0341dda3df3ea9428d5035
UmairFarooq614 <123209818+UmairFarooq614@users.noreply.github.com>
2023-02-09 15:40:38 +0500

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
+import { AppStateProvider } from './store' 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import './style.css'
-import WEB from './views/w-e-b'
+import './teleporthq/style.css'
+import WEB from './teleporthq/pages/w-e-b'
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <Route component={WEB} exact path="/" />
-      </div>
+      <AppStateProvider>
+        <Route component={WEB} exact path="/w-e-b" />
+      </AppStateProvider>
     </Router>
   )
 }
---
+++ src/teleporthq/pages/w-e-b.js
@@ -0,0 +1,278 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './w-e-b.css'
+
+const WEB = (props) => {
+  return (
+    <div className="w-e-b-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="w-e-b-w-e-b">
+        <img
+          src="/playground_assets/back1svg434-vw8.svg"
+          alt="back1svg434"
+          className="w-e-b-back1svg"
+        />
+        <div className="w-e-b-orderpreview">
+          <div className="w-e-b-cart">
+            <div className="w-e-b-frame3">
+              <span className="w-e-b-text">
+                <span>
+                  5 $
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+              <span className="w-e-b-text02">
+                <span>Quantity 1</span>
+              </span>
+              <span className="w-e-b-text04">
+                <span>Product 4</span>
+              </span>
+              <img
+                src="/playground_assets/nut46872166608142931svg4317-n5e7.png"
+                alt="nut46872166608142931svg4317"
+                className="w-e-b-nut46872166608142931svg"
+              />
+            </div>
+            <div className="w-e-b-frame2">
+              <span className="w-e-b-text06">
+                <span>
+                  4 $
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+              <span className="w-e-b-text08">
+                <span>Quantity 4</span>
+              </span>
+              <span className="w-e-b-text10">
+                <span>Product 2</span>
+              </span>
+              <img
+                src="/playground_assets/goodskinclubni7gy570MMUunsplashsvg4322-0ngd.png"
+                alt="goodskinclubni7gy570MMUunsplashsvg4322"
+                className="w-e-b-goodskinclubni7gy570m-m-uunsplashsvg"
+              />
+            </div>
+            <div className="w-e-b-frame1">
+              <span className="w-e-b-text12">
+                <span>
+                  7 $
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+              <span className="w-e-b-text14">
+                <span>Quantity 2</span>
+              </span>
+              <span className="w-e-b-text16">
+                <span>Product 1</span>
+              </span>
+              <img
+                src="/playground_assets/suplifulsupplementsondemandsaiTWnxaYAcunsplashsvg4327-lrtc.png"
+                alt="suplifulsupplementsondemandsaiTWnxaYAcunsplashsvg4327"
+                className="w-e-b-suplifulsupplementsondemandsai-t-wnxa-y-acunsplashsvg"
+              />
+            </div>
+          </div>
+          <img
+            src="/playground_assets/Line14328-ru5h.svg"
+            alt="Line14328"
+            className="w-e-b-line1"
+          />
+          <span className="w-e-b-text18">
+            <span className="w-e-b-text19"> 35.00 $</span>
+          </span>
+          <span className="w-e-b-text20">
+            <span>Total</span>
+          </span>
+          <div className="w-e-b-emailinput">
+            <img
+              src="/playground_assets/Rectangle34332-as8.png"
+              alt="Rectangle34332"
+              className="w-e-b-rectangle3"
+            />
+            <span className="w-e-b-text22">
+              <span> Email</span>
+            </span>
+          </div>
+          <span className="w-e-b-text24">
+            <span>Order No # 123456</span>
+          </span>
+        </div>
+        <button className="w-e-b-button">
+          <img
+            src="/playground_assets/Rectangle54336-hwma.png"
+            alt="Rectangle54336"
+            className="w-e-b-rectangle5"
+          />
+          <span className="w-e-b-text26">
+            <span>Paid</span>
+          </span>
+        </button>
+        <div className="w-e-b-m1svg">
+          <img
+            src="/playground_assets/Rectangle94339-yk7m.png"
+            alt="Rectangle94339"
+            className="w-e-b-rectangle9"
+          />
+          <span className="w-e-b-text28">
+            <span>Monero 1</span>
+          </span>
+          <img
+            src="/playground_assets/image14341-7uwh.png"
+            alt="image14341"
+            className="w-e-b-image1"
+          />
+        </div>
+        <div className="w-e-b-m1svg1">
+          <img
+            src="/playground_assets/Rectangle94343-uar.png"
+            alt="Rectangle94343"
+            className="w-e-b-rectangle91"
+          />
+          <span className="w-e-b-text30">
+            <span>Monero 1</span>
+          </span>
+          <img
+            src="/playground_assets/image14345-ij32.png"
+            alt="image14345"
+            className="w-e-b-image11"
+          />
+        </div>
+        <div className="w-e-b-group1">
+          <div className="w-e-b-product4">
+            <img
+              src="/playground_assets/Rectangle94348-li7.png"
+              alt="Rectangle94348"
+              className="w-e-b-rectangle92"
+            />
+            <span className="w-e-b-text32">
+              <span>5 $</span>
+            </span>
+            <span className="w-e-b-text34">
+              <span>Product 4</span>
+            </span>
+            <img
+              src="/playground_assets/nut46872166608142931svg4351-iv4o.png"
+              alt="nut46872166608142931svg4351"
+              className="w-e-b-nut46872166608142931svg1"
+            />
+            <img
+              src="/playground_assets/SVGiconremovecartbutton4352-7j8.svg"
+              alt="SVGiconremovecartbutton4352"
+              className="w-e-b-s-v-giconremovecartbutton"
+            />
+            <img
+              src="/playground_assets/SVGiconaddcartbutton4353-6ogt.svg"
+              alt="SVGiconaddcartbutton4353"
+              className="w-e-b-s-v-giconaddcartbutton"
+            />
+          </div>
+          <div className="w-e-b-product3">
+            <img
+              src="/playground_assets/Rectangle94355-sjdh.png"
+              alt="Rectangle94355"
+              className="w-e-b-rectangle93"
+            />
+            <span className="w-e-b-text36">
+              <span>9 $</span>
+            </span>
+            <span className="w-e-b-text38">
+              <span>Product 3</span>
+            </span>
+            <img
+              src="/playground_assets/suplifulsupplementsondemandURH9F9E32ogunsplashsvg4358-67ig.png"
+              alt="suplifulsupplementsondemandURH9F9E32ogunsplashsvg4358"
+              className="w-e-b-suplifulsupplementsondemand-u-r-h9f9e32ogunsplashsvg"
+            />
+            <img
+              src="/playground_assets/SVGiconremovecartbutton4359-qj9k.svg"
+              alt="SVGiconremovecartbutton4359"
+              className="w-e-b-s-v-giconremovecartbutton1"
+            />
+            <img
+              src="/playground_assets/SVGiconaddcartbutton4360-vxn7.svg"
+              alt="SVGiconaddcartbutton4360"
+              className="w-e-b-s-v-giconaddcartbutton1"
+            />
+          </div>
+          <div className="w-e-b-product2">
+            <img
+              src="/playground_assets/Rectangle94362-jwh.png"
+              alt="Rectangle94362"
+              className="w-e-b-rectangle94"
+            />
+            <span className="w-e-b-text40">
+              <span>4 $</span>
+            </span>
+            <span className="w-e-b-text42">
+              <span>Product 2</span>
+            </span>
+            <img
+              src="/playground_assets/goodskinclubni7gy570MMUunsplashsvg4365-3oal.png"
+              alt="goodskinclubni7gy570MMUunsplashsvg4365"
+              className="w-e-b-goodskinclubni7gy570m-m-uunsplashsvg1"
+            />
+            <img
+              src="/playground_assets/SVGiconremovecartbutton4366-034j.svg"
+              alt="SVGiconremovecartbutton4366"
+              className="w-e-b-s-v-giconremovecartbutton2"
+            />
+            <img
+              src="/playground_assets/SVGiconaddcartbutton4367-ntdg.svg"
+              alt="SVGiconaddcartbutton4367"
+              className="w-e-b-s-v-giconaddcartbutton2"
+            />
+          </div>
+          <div className="w-e-b-product1">
+            <img
+              src="/playground_assets/Rectangle94369-snr.png"
+              alt="Rectangle94369"
+              className="w-e-b-rectangle95"
+            />
+            <span className="w-e-b-text44">
+              <span>7 $</span>
+            </span>
+            <span className="w-e-b-text46">
+              <span>Product 1</span>
+            </span>
+            <img
+              src="/playground_assets/suplifulsupplementsondemandsaiTWnxaYAcunsplashsvg4372-52qp.png"
+              alt="suplifulsupplementsondemandsaiTWnxaYAcunsplashsvg4372"
+              className="w-e-b-suplifulsupplementsondemandsai-t-wnxa-y-acunsplashsvg1"
+            />
+            <img
+              src="/playground_assets/SVGiconremovecartbutton4373-vhz.svg"
+              alt="SVGiconremovecartbutton4373"
+              className="w-e-b-s-v-giconremovecartbutton3"
+            />
+            <img
+              src="/playground_assets/SVGiconaddcartbutton4378-55sr.svg"
+              alt="SVGiconaddcartbutton4378"
+              className="w-e-b-s-v-giconaddcartbutton3"
+            />
+          </div>
+        </div>
+        <span className="w-e-b-text48">
+          <span>Site Name</span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default WEB
